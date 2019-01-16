d1723aa11fa493af2a35e6268bd0a8faa8db7df9
RyndychRD <r.roman1234@yandex.ru>
2019-01-16 10:20:55 +1000

    project

--- prog/app/src/main/java/com/example/project/MainActivity.java
+++ prog/app/src/main/java/com/example/project/MainActivity.java
@@ -22,25 +22,27 @@ public class MainActivity extends AppCompatActivity {
         goto_choose_color = findViewById(R.id.goto_learning_color);
         goto_choose_figure = findViewById(R.id.goto_learning_figure);
 
-        View.OnClickListener oclButton1=new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, settings.class);
-                startActivity(intent);
+            View.OnClickListener oclButton1 = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, settings.class);
+                    startActivity(intent);
 
-            }
-        };
-        goto_settings.setOnClickListener(oclButton1);
+                }
+            };
+            goto_settings.setOnClickListener(oclButton1);
 
-        View.OnClickListener oclButton2=new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, choose_color.class);
-                startActivity(intent);
+            View.OnClickListener oclButton2 = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, learning_color.class);
+                    startActivity(intent);
 
-            }
-        };
-        goto_choose_color.setOnClickListener(oclButton2);
+                }
+            };
+            goto_choose_color.setOnClickListener(oclButton2);
+
+
     }
 
 
--- prog/app/src/main/java/com/example/project/learning_color.java
+++ prog/app/src/main/java/com/example/project/learning_color.java
@@ -1,20 +1,40 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class learning_color extends AppCompatActivity {
-
+    SoundPool sp;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_color);
-        
+        next = findViewById(R.id.goto_play_choose_color);
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+        for (int i=1;i<11;i++){
+            final int soundIdRight;
+            soundIdRight = sp.load(this,getResources().getIdentifier("sound_learning_color_"+i,"drawable",getPackageName()),1);
+            sp.play(soundIdRight,1,1,0,2,1);
+
+
+            View.OnClickListener oclButton1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(learning_color.this, choose_color.class);
+                startActivity(intent);
+
+            }
+        };
+        next.setOnClickListener(oclButton1);
+
+        }
     }
-    public void onClick(View view) {
-        Intent intent = new Intent(learning_color.this, choose_color.class);
-        startActivity(intent);
-    }
+
 }
